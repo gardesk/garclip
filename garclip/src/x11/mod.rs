cb46591dc8d220657df9371b531a4282701484e4
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-17 06:13:52 -0500

    export window_info from x11 module

--- garclip/src/x11/mod.rs
+++ garclip/src/x11/mod.rs
@@ -1,7 +1,9 @@
 pub mod atoms;
 pub mod selection;
 pub mod transfer;
+pub mod window_info;
 
 pub use atoms::Atoms;
 pub use selection::SelectionManager;
 pub use transfer::TransferManager;
+pub use window_info::{get_window_class, get_wm_class};
