9a4c04cc872d79cf6b9dc4d118883ba87161e36f
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-17 09:04:38 -0500

    fix serde tagged enum for ClipboardContent

--- garclip/src/clipboard/entry.rs
+++ garclip/src/clipboard/entry.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 /// Content stored in the clipboard
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type", rename_all = "snake_case")]
+#[serde(tag = "type", content = "data", rename_all = "snake_case")]
 pub enum ClipboardContent {
     /// Plain text content
     Text(String),
