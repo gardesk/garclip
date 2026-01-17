a20467126931ef039bbed46ae6eec64bdec04e23
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-17 06:14:00 -0500

    export GarClient from ipc module

--- garclip/src/ipc/mod.rs
+++ garclip/src/ipc/mod.rs
@@ -1,7 +1,9 @@
 pub mod client;
+pub mod gar_client;
 pub mod protocol;
 pub mod server;
 
 pub use client::{send_command, send_command_blocking};
+pub use gar_client::GarClient;
 pub use protocol::{Command, Event, Response};
 pub use server::{IpcClient, IpcServer};
