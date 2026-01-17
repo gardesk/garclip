d921718a9116afff1dd515ec27f1844054e80082
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-17 06:05:45 -0500

    export ContentFilter from clipboard module

--- garclip/src/clipboard/mod.rs
+++ garclip/src/clipboard/mod.rs
@@ -1,7 +1,9 @@
 pub mod entry;
+pub mod filter;
 pub mod history;
 pub mod manager;
 
 pub use entry::{ClipboardContent, ClipboardEntry};
+pub use filter::ContentFilter;
 pub use history::ClipboardHistory;
 pub use manager::ClipboardManager;
